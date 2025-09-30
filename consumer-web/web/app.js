bb59418710a2597089ddd1808f6a1b5796f2320a
Lewis P <prescottlewis707@gmail.com>
2025-09-30 12:54:21 +0100

    chore: modify workshop error

--- consumer-web/web/app.js
+++ consumer-web/web/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/web.html'));
 
-  app.get('/movies.json', (req, res) => {
+  app.get('/movie/100', (req, res) => {
   res.sendFile(path.join(__dirname, '/movies.json'));
 });
 });
